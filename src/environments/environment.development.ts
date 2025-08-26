3c7260936ed7afd516c31ae4de6e99f1729da126
Maheshkumar s <>
2025-08-26 16:21:18 +0530

    environment changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,9 +2,9 @@ export const environment = {
     production: false,
     // apiUrl: 'http://91.108.110.35:5001',
     // apiUrl: 'https://medicuity.com',
-    codingApiUrl: 'http://48.217.188.56:6002',
-    redactApiUrl: 'http://48.217.188.56:6001',
-    // codingApiUrl: 'https://coding.medicuity.com',
+    // codingApiUrl: 'http://48.217.188.56:6002',
+    // redactApiUrl: 'http://48.217.188.56:6001',
+    codingApiUrl: 'https://coding.medicuity.com',
     // codingApiUrl: 'http://127.0.0.1:5001',
-    // redactApiUrl: 'https://redaction.medicuity.com',
+    redactApiUrl: 'https://redaction.medicuity.com',
 };
